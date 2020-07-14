6d001ec2da42db564ee1b54f147f350858de1240
YechengChu <szcyc27@163.com>
2020-07-14 19:53:18 +0800

    first commit

---
+++ proj1/routes.go
@@ -0,0 +1,64 @@
+// routes.go
+
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func initializeRoutes() {
+
+	// Use the setUserStatus middleware for every route to set a flag
+	// indicating whether the request was from an authenticated user or not
+	router.Use(setUserStatus())
+
+	// Handle the index route
+	router.GET("/", showIndexPage)
+	// router.GET("/", func(c *gin.Context) {
+	//
+	// 	// Call the HTML method of the Context to render a template
+	// 	c.HTML(
+	// 		// Set the HTTP status to 200 (OK)
+	// 		http.StatusOK,
+	// 		// Use the index.html template
+	// 		"index.html",
+	// 		// Pass the data that the page uses (in this case, 'title')
+	// 		gin.H{
+	// 			"title": "Home Page",
+	// 		},
+	// 	)
+	// })
+
+	// Group user related routes together
+	userRoutes := router.Group("/")
+	{
+		// Handle the GET requests at /u/login
+		// Show the login page
+		// Ensure that the user is not logged in by using the middleware
+		userRoutes.GET("signin", ensureNotLoggedIn(), showLoginPage)
+
+		// Handle POST requests at /u/login
+		// Ensure that the user is not logged in by using the middleware
+		userRoutes.POST("profile", ensureNotLoggedIn(), performLogin)
+
+		// Handle GET requests at /u/logut
+		// Ensure that the user is logged in by using the middleware
+		userRoutes.GET("logout", ensureLoggedIn(), logout)
+
+		// Handle the GET requests at /u/register
+		// Show the registration page
+		// Ensure that the user is not logged in by using the middleware
+		userRoutes.GET("signup", ensureNotLoggedIn(), showRegistrationPage)
+
+		// Handle POST requests at /u/register
+		// Ensure that the user is not logged in by using the middleware
+		userRoutes.POST("signup", ensureNotLoggedIn(), register)
+	}
+}
+
+func showIndexPage(c *gin.Context) {
+	// Call the render function with the name of the template to render
+	render(c, gin.H{
+		"title": "Home Page",
+	}, "index.html")
+}
